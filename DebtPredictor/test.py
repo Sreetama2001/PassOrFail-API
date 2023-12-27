3fa2c04f57a43fcb28fd2af04f8df2e4da10010e
Sreetama <fool100abc@gmail.com>
2023-12-27 23:12:21 +0530

    Added model on Debt predictor

--- DebtPredictor/test.py
+++ DebtPredictor/test.py
@@ -7,8 +7,8 @@ with open('DebtPredictor/model_debt.pkl', 'rb') as f:
 
 new_data = {
     'Loan_Amount': [10000],
-    'Shopping_Expenses': [800],
-    'Travel_Expenses': [200],
+    'Shopping_Expenses': [8000],
+    'Travel_Expenses': [200000],
     'Movie_Expenses': [10],
     'Dining_Expenses': [250],
     'Entertainment_Expenses': [150],
@@ -18,7 +18,7 @@ new_data = {
 new_df = pd.DataFrame(new_data)
 
 # Make predictions using the loaded model
-predictions = model.predict(new_df)
-probabilty = np.max(model.predict_proba(new_df))
-print(predictions)
-print(probabilty)
+predictions = model.predict(new_df) # binary 
+probabilty = np.max(model.predict_proba(new_df)) # 0 - 1 decimal
+print(predictions) # class value 1 or 0 
+print(probabilty) # class 
